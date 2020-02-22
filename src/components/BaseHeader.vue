7e04d82c48f77b59639ea07063edae799d400bd7
KenrickDietrich <kenrickdietrich@gmail.com>
2020-02-22 16:09:59 +0100

    First commit

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.test">This is a test</div>
+</template>
+
+<script>
+export default {
+  name: "BaseHeader"
+};
+</script>
+
+<style lang="scss" module>
+.test {
+  background-color: $color-primary;
+  height: 100px;
+  width: 1000px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <base-header></base-header>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import BaseHeader from "@/components/BaseHeader.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    BaseHeader
   }
 };
 </script>
