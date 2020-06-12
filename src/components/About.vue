5614a659167c19e170a0d9313e37dd95082250b2
KenrickDietrich <kenrickdietrich@gmail.com>
2020-06-12 12:29:25 +0200

    fixed about.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,24 +1,21 @@
 <template>
-  <section id="about">
+  <section id="about" :class="$style.about">
     <b-container class="container" fluid>
-      <div class="row-container">
-        <b-row>
-          <b-col>
-            <img
-              src="../assets/about.jpg"
-              alt="image"
-              class="img-fluid"
-              id="about-img"
-            />
-          </b-col>
-          <b-col>
-            <div>
-              <h1>Wie ik ben</h1>
-              <p>Dit is de paragraaf met informatie text</p>
-            </div>
-          </b-col>
-        </b-row>
-      </div>
+      <b-row>
+        <b-col>
+          <img
+            src="../assets/about.jpg"
+            alt="image"
+            class="img-fluid"
+          />
+        </b-col>
+        <b-col>
+          <div>
+            <h1>Wie ik ben</h1>
+            <p>Dit is de paragraaf met informatie text</p>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
   </section>
 </template>
@@ -28,3 +25,19 @@ export default {
   name: 'About',
 };
 </script>
+
+<style lang="scss" module>
+.about {
+  position: relative;
+  padding-top: 80px;
+  padding-bottom: 80px;
+  text-align: center;
+  width: 100%;
+  display: inline-block;
+
+  img {
+    max-width: 70%;
+    border-radius: $default-border-radius;
+  }
+}
+</style>
